8a7b1de2b332a9b8f0d7d62d158ca0d03d68a614
ALeX <alexdeoliveira.poli@gmail.com>
2021-11-11 08:46:44 -0300

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Alex Oliveira on 11/11/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
