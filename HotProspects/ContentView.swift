d8afb16e551bac908bf51bd52f37248eaae1dd5f
ALeX <alexdeoliveira.poli@gmail.com>
2021-11-11 09:41:13 -0300

    Creating tabs with TabView and tabItem() – Hot Prospects SwiftUI Tutorial 2/16

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,36 +7,32 @@
 
 import SwiftUI
 
-class User: ObservableObject {
-    @Published var name = "Taylor Swift"
-}
-
-struct EditView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        TextField("Name", text: $user.name)
-    }
-}
-
-struct DisplayView: View {
-    @EnvironmentObject var user: User
-    
-    var body: some View {
-        Text(user.name)
-    }
-}
-
 struct ContentView: View {
-    let user = User()
+    @State private var selectedTab = 0
     
     var body: some View {
-        VStack {
-            // Children of ContentView
-            EditView()
-            DisplayView()
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .padding()
+                .onTapGesture {
+                    self.selectedTab = 1
+                }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("One")
+                }
+            
+            Text("Tab 2")
+                .padding()
+                .onTapGesture {
+                    self.selectedTab = 100 // Any integer makes switch tabs work, as long as it's not 1, because 1 was set for this tab with the tag modifier
+                }
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+                }
+                .tag(1)
         }
-        .environmentObject(user)
     }
 }
 
