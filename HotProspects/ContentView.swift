36f2737fbb5a2968a3e812f4da88cb5121bb5b7b
ALeX <alexdeoliveira.poli@gmail.com>
2021-11-12 18:58:13 -0300

    Controlling image interpolation in SwiftUI – Hot Prospects SwiftUI Tutorial 5/16

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,28 +7,15 @@
 
 import SwiftUI
 
-class DelayedUpdater: ObservableObject {
-    var value = 0 {
-       willSet {
-           objectWillChange.send()
-       }
-    }
-    
-    init() {
-        for i in 1...10 {
-            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
-                self.value += 1
-            }
-        }
-    }
-}
-
 struct ContentView: View {
-    @ObservedObject var updater = DelayedUpdater()
-    
     var body: some View {
-        Text("Value is: \(updater.value)")
-            .padding()
+        Image("example")
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(maxHeight: .infinity)
+            .background(Color.black)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
