b371632baf6243b4b44b640772520f19eb47e33a
ALeX <alexdeoliveira.poli@gmail.com>
2021-11-12 18:44:00 -0300

    Manually publishing ObservableObject changes – Hot Prospects SwiftUI Tutorial 4/16

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,51 +7,28 @@
 
 import SwiftUI
 
-enum NetworkError: Error {
-    case badURL, requestFailed, unkown
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+       willSet {
+           objectWillChange.send()
+       }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
 }
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-            .padding()
-            .onAppear {
-                self.fetchData(from: "https://www.apple.com") { result in
-                    switch result {
-                    case .success(let str):
-                        print(str)
-                    case .failure(let error):
-                        switch error {
-                        case .badURL:
-                            print("Bad URL")
-                        case .requestFailed:
-                            print("Network problems")
-                        case .unkown:
-                            print("Unknown error")
-                        }
-                    }
-                }
-            }
-    }
+    @ObservedObject var updater = DelayedUpdater()
     
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        guard let url = URL(string: urlString) else {
-            completion(.failure(.badURL))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            DispatchQueue.main.async {
-                if let data = data {
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    completion(.failure(.requestFailed))
-                } else {
-                    completion(.failure(.unkown))
-                }
-            }
-        }.resume()
+    var body: some View {
+        Text("Value is: \(updater.value)")
+            .padding()
     }
 }
 
